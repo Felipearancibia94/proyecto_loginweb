00ba73c4dbebf91b9bb834f2a4e4b379c78be730
Felipe <felipearancibiac94@gmail.com>
2021-02-23 20:13:45 -0300

    Version con login

--- src/routes/links.js
+++ src/routes/links.js
@@ -5,8 +5,11 @@ const pool = require('../database');
 
 
 router.get('/add', (req, res) => {
-    res.send('Form');
+    res.render('links/add');
 });
 
+router.post('/add', (req, res) => {
+    res.send('received');
+})
 
 module.exports = router;
